59ca9adafa549fb016723327187d9992ebf11425
Jakob <Jakob@DESKTOP-E0F5V8S.fritz.box>
2022-10-27 15:23:25 +0200

    task 3, exercise 1 final commit

---
+++ HW3.1/src/de/uni/koeln/se/state/Elevator.java
@@ -0,0 +1,57 @@
+package de.uni.koeln.se.state;
+
+public class Elevator {
+
+	public static int current_Floor = 5;
+	int dest_Floor;
+
+	private Elv_States State = new Elv_States();
+
+	public Elevator(int dest_floor) {
+		this.dest_Floor = dest_floor;
+		System.out.println("*** Destination floor is: " + dest_Floor);
+		arrive_atFloor();
+
+	}
+
+	private void arrive_atFloor() {
+		if (current_Floor > dest_Floor) {
+			State.Current_State = State.Moving_down;
+			System.out.println(State.Current_State);
+			current_Floor--;
+			System.out.println(String.valueOf(current_Floor));
+			while (current_Floor > dest_Floor) {
+				current_Floor--;
+				System.out.println(String.valueOf(current_Floor));
+			}
+			if (current_Floor == dest_Floor) {
+				State.Current_State = State.Arrive_to_Floor;
+			}
+		}
+		
+		if (current_Floor < dest_Floor) {
+			State.Current_State = State.Moving_up;
+			System.out.println(State.Current_State);
+			current_Floor++;
+			System.out.println(String.valueOf(current_Floor));
+			while (current_Floor < dest_Floor) {
+				current_Floor++;
+				System.out.println(String.valueOf(current_Floor));
+			}
+			if (current_Floor == dest_Floor) {
+				State.Current_State = State.Arrive_to_Floor;
+				
+			}
+		}
+		
+		if (current_Floor == dest_Floor) {
+			State.Current_State = State.Exit;
+			System.out.println(State.Current_State);
+		}
+		
+
+		System.out.println("Arrived at Floor " + String.valueOf(current_Floor));
+
+	}
+
+}
---
+++ HW3.1/src/de/uni/koeln/se/state/Elv_States.java
@@ -0,0 +1,16 @@
+package de.uni.koeln.se.state;
+
+public class Elv_States {
+	
+	public String Current_State;
+	
+	public String Moving_up = "Moving_up";
+
+	public String Moving_down = "Moving_down";
+	
+	public String Exit = "Exit";
+	
+	public String Arrive_to_Floor = "Arrive_to_Floor";
+	
+
+}
