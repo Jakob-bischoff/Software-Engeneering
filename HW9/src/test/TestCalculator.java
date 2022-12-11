6a860c0784f4a4832277d3a3d4076cadd449e4fc
Jakob <Jakob@DESKTOP-E0F5V8S.fritz.box>
2022-12-11 19:12:16 +0100

    HW9 done

---
+++ HW9/src/test/TestCalculator.java
@@ -0,0 +1,46 @@
+package test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import main.Calculator;
+
+
+class TestCalculator {
+
+	Calculator calculator = new Calculator();
+
+	@Test
+	public void Should_add_two_numbers_and_return_result() {
+		  int expected =  5+2 ;
+	        int actual = calculator.add(5,2) ;
+	        assertEquals(expected,actual) ;
+	}
+	
+	@Test
+	public void Should_substract_two_numbers_and_return_result() {
+		   int expected = 10-3  ;
+	        int actual = calculator.sub(10,3) ;
+	        assertEquals(expected,actual);
+	}
+	
+	@Test
+	public void Should_multiply_two_numbers_and_return_result() {
+		  int expected = 6*7 ;
+	        int actual = calculator.multiply(6,7) ;
+	        assertEquals(expected,actual);
+	}
+	
+	@Test
+	public void Should_divide_two_numbers_and_return_result() {
+		 float expected = 10/5 ;
+	        float actual  =  calculator.divide(10,5) ;
+	        assertEquals(expected,actual);
+	}
+	
+	@Test
+	public void Should_throw_an_arithmetic_exception_if_denominator_is_zero() {
+		Throwable e = assertThrows(ArithmeticException.class,() -> calculator.divide(20,0));
+	    assertEquals("ZeroDivisionError", e.getMessage());
+
+	}
+}
